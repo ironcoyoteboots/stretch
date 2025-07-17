97070e7b37896c11dd67e14ba132b297766c28c2
ironcoyoteboots <jeremiahparsons@gmail.com>
2025-07-17 15:11:57 -0700

    quick update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,8 +6,8 @@ export default function Header() {
     return (
         <header className="fixed top-0 left-0 w-full z-20 bg-white bg-opacity-90 backdrop-blur-md shadow-sm">
             <div className="max-w-7xl mx-auto px-4 py-3 flex justify-between items-center">
-                <div className="text-3xl text-gray-800 uppercase">
-                    <span className="font-extrabold">BIG</span><span className="font-light">STRETCH</span>
+                <div className="text-3xl text-gray-800">
+                    <span className="font-extrabold">big</span><span className="font-light">stretch</span>
                 </div>
                 <nav className="space-x-4 text-gray-700 uppercase">
                     <a href="#" className="hover:text-cyan-600 align-middle">Services</a>
